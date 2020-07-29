62e608174b1a47657b56fe278b3362a2d1aecc95
Arindam Roy <arindamroy@Arindams-MacBook-Pro.local>
2020-07-29 22:18:52 +0530

    raw json

--- handlers.go
+++ handlers.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"aa-hackathon/models"
+	"io/ioutil"
 	"net/http"
 )
 import jsoniter "github.com/json-iterator/go"
@@ -19,41 +19,41 @@ func handleDataflowUpdate(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	var dataflowUpdate models.DataFlowUpdate
-	err := json.NewDecoder(request.Body).Decode(&dataflowUpdate)
-
-	if err != nil {
-		ErrorLogger.Printf("Error in decoding dataflow update; err=%v \n", err)
-		writer.WriteHeader(400)
-		return
-	}
-
-	DataflowLogger.Printf("%+v", dataflowUpdate)
+	d, _:= ioutil.ReadAll(request.Body)
+	DataflowLogger.Printf("%v", d)
+	//var dataflowUpdate models.DataFlowUpdate
+	//err := json.NewDecoder(request.Body).Decode(&dataflowUpdate)
+	//
+	//if err != nil {
+	//	ErrorLogger.Printf("Error in decoding dataflow update; err=%v \n", err)
+	//	writer.WriteHeader(400)
+	//	return
+	//}
+	//
+	//DataflowLogger.Printf("%+v", dataflowUpdate)
 	writer.WriteHeader(200)
 }
 
 func handleConsentUpdate(writer http.ResponseWriter, request *http.Request) {
 
-	if request.Method != http.MethodPost {
-		writer.WriteHeader(401)
-		return
-	}
 
 	if request.Method != http.MethodPost {
 		writer.WriteHeader(401)
 		return
 	}
 
-	var consentUpdate models.ConsentUpdate
-	err := json.NewDecoder(request.Body).Decode(&consentUpdate)
-
-	if err != nil {
-		ErrorLogger.Printf("Error in decoding consent update; err=%v \n", err)
-		writer.WriteHeader(400)
-		return
-	}
-
-	ConsentLogger.Printf("%+v", consentUpdate)
+	c, _:= ioutil.ReadAll(request.Body)
+	ConsentLogger.Printf("%v", c)
+	//var consentUpdate models.ConsentUpdate
+	//err := json.NewDecoder(request.Body).Decode(&consentUpdate)
+	//
+	//if err != nil {
+	//	ErrorLogger.Printf("Error in decoding consent update; err=%v \n", err)
+	//	writer.WriteHeader(400)
+	//	return
+	//}
+	//
+	//ConsentLogger.Printf("%+v", consentUpdate)
 	writer.WriteHeader(200)
 
 }
